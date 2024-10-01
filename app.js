1b9a2c97d9d1ee39b69a028b7f73514bd32a7155
xsaxdax <60689251+xsaxdax@users.noreply.github.com>
2024-10-01 16:12:10 +0200

    dynamic search

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const displayIdNumber =  (i) => {
-    switch (true) {
-      case (i < 9):
-        return "#00" + (+i + 1);
-      case (i < 99):
-        return "#0" + (+i + 1)
-      default:
-        return "#" + (+i + 1);
-    }
+const displayIdNumber = (i) => {
+  switch (true) {
+    case (i < 9):
+      return "#00" + (+i + 1);
+    case (i < 99):
+      return "#0" + (+i + 1)
+    default:
+      return "#" + (+i + 1);
   }
+}
 
 const cardTemplate = (id, name) => {
   return `
-  <div class="card ${name}" height="108" width="104" style="overflow: hidden; border:1px solid silver; border-radius: 8%; background: #FFFFFF;">
+  <div class="card " id="${name}" height="108" width="104" style="overflow: hidden; border:1px solid silver; border-radius: 8%; background: #FFFFFF;">
         <p style="font-size: 8px; padding-top:4px;  text-align:right;padding-right: 8px; height: 14px; margin-bottom: 0px; color: #666666; ">${displayIdNumber(id)}</p>
           <img class="pokemon-image" loading="lazy" src="/assets/${+id + 1}.png" alt="${name}" style="width: 72px;height: 72px; z-index: 1; margin-bottom: -26px;" />
           <div style=" height:44px; z-index: 0; border-radius: 7%; background-color: #E0E0E0; ">
@@ -22,12 +22,18 @@ const cardTemplate = (id, name) => {
     `
 }
 
+const setAllVisible = () => {
+  document.querySelectorAll('.card')
+    .forEach(element => {
+      element.style.display = ""
+    })
+}
 
 window.addEventListener('DOMContentLoaded', function () {
 
   const info = document.querySelector(".info-d");
   let referenceElement = document.getElementById('card-wrapper');
-         
+
   const testMap = new Map([
     [
       "bulbasaur",
@@ -4133,11 +4139,10 @@ window.addEventListener('DOMContentLoaded', function () {
   new Autocomplete("static", {
     selectFirst: true,
     removeResultsWhenInputIsEmpty: true,
+    preventScrollUp: true,  
     onSearch: ({ currentValue }) => {
       // static file
       const api = "/Pokemon_Names_Type.json";
-      //let toRemoveFromView = 
-      //document.getElementsByClassName('card')
       return new Promise((resolve) => {
         fetch(api)
           .then((response) => response.json())
@@ -4145,10 +4150,9 @@ window.addEventListener('DOMContentLoaded', function () {
             const result = data
               .sort((a, b) => a.name.localeCompare(b.name))
               .filter((element) => {
-
                 return element.name.match(new RegExp('^' + currentValue, "i"));
-              });
-            let t = cardTemplate(result[0].ID, result[0].name)
+              })
+
             resolve(result);
           })
           .catch((error) => {
@@ -4158,25 +4162,44 @@ window.addEventListener('DOMContentLoaded', function () {
     },
 
     onResults: ({ currentValue, matches }) => {
-      // checking if we have results if we don't
-      // take data from the noResults callback
+      let remove = matches.map((item) => "#" + item['name'])
+
+      setAllVisible()
+      //select all the cards that werent found by user input search
+      //and apply display hidden
+      document.querySelectorAll('.card:not(' + remove + ')')
+        .forEach(element => {
+          element.style.display = "none"
+        })
+
+     
+
       return matches.map((el) => {
-            return `
+        return `
             <li>
               <p>${el.name.replace(
-              new RegExp(currentValue, "gi"),
-              (str) => `<b>${str}</b>`
-            )}</p>
+          new RegExp(currentValue, "gi"),
+          (str) => `<b>${str}</b>`
+        )}</p>
             </li>`;
-          })
-          .join("");
+      })
+        .join("");
     },
-    
+
 
 
     // event onsubmit
     onSubmit: ({ index, element, object }) => {
       const { name, ID, status, img } = object;
+      //setAllVisible()
+
+      document.getElementsByClassName("auto-results-wrapper auto-is-active")[0].classList.remove('auto-is-active');
+      document.querySelectorAll('.card:not(#' + name + ')')
+        .forEach(element => {
+          element.style.display = "none"
+        })
+      /*
+      
 
       console.table("static-file-data", index, element, object);
 
@@ -4194,6 +4217,7 @@ window.addEventListener('DOMContentLoaded', function () {
       info.textContent = "";
       info.classList.add("active-data");
       info.insertAdjacentHTML("beforeend", template);
+      */
     },
 
     // get index and data from li element after
@@ -4205,6 +4229,7 @@ window.addEventListener('DOMContentLoaded', function () {
     onReset: () => {
       info.textContent = "";
       info.classList.remove("active-data");
+      setAllVisible()
     },
 
 
